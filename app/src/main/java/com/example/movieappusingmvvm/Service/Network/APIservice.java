53518bd3d7815c19a9235b9c984c5ee79fbbbfeb
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2023-06-02 01:32:15 +0600

    Api interface 2

--- app/src/main/java/com/example/movieappusingmvvm/Service/Network/APIservice.java
+++ app/src/main/java/com/example/movieappusingmvvm/Service/Network/APIservice.java
@@ -4,11 +4,10 @@ import com.example.movieappusingmvvm.Service.Model.MovieModel;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
 
 public interface APIservice {
 
 
-    @GET("3/movie/top_rated?api_key='your_API_key")
+    @GET("3/movie/top_rated?api_key='")
     Call<MovieModel>getMovieList();
 }
