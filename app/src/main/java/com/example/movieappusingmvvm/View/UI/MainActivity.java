53597a4faf3199d2f2c4d2427ed1bd050e7a71fa
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2023-06-02 19:40:52 +0600

    UI completed

--- app/src/main/java/com/example/movieappusingmvvm/View/UI/MainActivity.java
+++ app/src/main/java/com/example/movieappusingmvvm/View/UI/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.movieappusingmvvm.View.UI;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.movieappusingmvvm.R;
 import com.example.movieappusingmvvm.Service.Model.Result;
 import com.example.movieappusingmvvm.Service.Repository.MovieRepository;
+import com.example.movieappusingmvvm.View.Adapter.movieAdapter;
 import com.example.movieappusingmvvm.ViewModel.MovieListViewModel;
 
 import java.util.List;
@@ -17,16 +20,21 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private MovieListViewModel mViewModel;
+    private movieAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         recyclerView=findViewById(R.id.recyle);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
         mViewModel=new ViewModelProvider(this).get(MovieListViewModel.class);
         mViewModel.getTopRatedMovieLists().observe(this, new Observer<List<Result>>() {
             @Override
             public void onChanged(List<Result> results) {
+           adapter=new movieAdapter(MainActivity.this,results);
+           recyclerView.setAdapter(adapter);
+
 
             }
         });
